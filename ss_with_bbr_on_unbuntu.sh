a5704e707556975ec87fa5a3a3d965685184d950
wuke.wk <wuke.wk@alibaba-inc.com>
2019-08-21 11:34:31 +0800

    add ssr install script

--- ss_with_bbr_on_unbuntu.sh
+++ ss_with_bbr_on_unbuntu.sh
@@ -10,3 +10,9 @@ echo 'net.ipv4.tcp_congestion_control=bbr' > /etc/sysctl.conf
 sudo sysctl -p
 sysctl net.ipv4.tcp_congestion_control
 
+# install shadowsocks-R
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+chmod +x shadowsocksR.sh
+./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+
+/etc/init.d/shadowsocks restart
